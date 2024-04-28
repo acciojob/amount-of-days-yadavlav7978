e1f57eb1331980b6c75ddec07610d1a81641ce8e
Lav Yadav <yadavlav571@gmail.com>
2024-04-28 11:25:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+
+let daysOfAYear=(n)=>{
+	if(n%400==0 || (n%4==0 && n%100!=0)) return true;
+	return false;
+}
+
+
+let value = prompt("Enter the year.");
+
+alert(daysOfAYear(value));
