4443d68e54726bd5e14b11e86982f8a6147bfd22
Lav Yadav <yadavlav571@gmail.com>
2024-04-28 11:28:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //your JS code here. If required.
 
 let daysOfAYear=(n)=>{
-	if(n%400==0 || (n%4==0 && n%100!=0)) return true;
-	return false;
+	if(n%400==0 || (n%4==0 && n%100!=0)) return 366;
+	return 365;
 }
 
 
